3663fcc3170e298537050777dd4e4c2760ddfc16
Lee Codingcow <codingcow@163.com>
2025-03-01 12:03:10 +0800

    Filemanage backend finished

--- service/common/__init__.py
+++ service/common/__init__.py
@@ -1,1 +1,12 @@
 from . import db
+
+import hashlib
+
+
+def get_file_hash(filename: str) -> str:
+    """
+    Get file hash
+    获取文件哈希
+    """
+    with open(filename, "rb") as f:
+        return hashlib.md5(f.read()).hexdigest()
---
+++ service/filemanage.py
@@ -0,0 +1,172 @@
+# filemanage.py
+from fastapi import APIRouter, HTTPException, Cookie
+from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
+from typing import Union
+import minio
+import oss2
+
+import os
+
+import common
+
+
+router = APIRouter()
+db = common.db.get_database()
+system_config = common.db.get_system_config()
+
+
+class Storage:
+    def __init__(self, url: str, access_key: str, secret_key: str, bucket: str): ...
+
+    def upload(self, file: bytes, filename: str): ...
+
+    def download(self, filename: str): ...
+
+    def get_url(self, filename: str): ...
+
+    def delete(self, filename: str): ...
+
+    def generate_presigned_url(self, filename: str, expires: int = 3600): ...
+
+
+class MinIOStorage(Storage):
+    def __init__(self, url: str, access_key: str, secret_key: str, bucket: str):
+        self.client = minio.Minio(url, access_key, secret_key)
+        self.bucket = bucket
+
+    def upload(self, file: bytes, filename: str):
+        self.client.put_object(bucket_name=self.bucket, object_name=filename, data=file)
+
+    def download(self, filename: str):
+        return self.client.get_object(bucket_name=self.bucket, object_name=filename)
+
+    def delete(self, filename: str):
+        self.client.remove_object(bucket_name=self.bucket, object_name=filename)
+
+    def generate_presigned_url(self, filename: str, expires: int = 3600):
+        return self.client.presigned_get_object(self.bucket, filename, expires)
+
+
+class OSSStorage(Storage):
+    def __init__(self, url: str, access_key: str, secret_key: str, bucket: str):
+        self.client = oss2.Bucket(oss2.Auth(access_key, secret_key), url, bucket)
+        self.url = url
+
+    def upload(self, file: bytes, filename: str):
+        self.client.put_object(filename, file)
+
+    def download(self, filename: str):
+        return self.client.get_object(filename)
+
+    def delete(self, filename: str):
+        self.client.delete_object(filename)
+
+    def generate_presigned_url(self, filename: str, expires: int = 3600):
+        return self.client.sign_url("GET", filename, expires)
+
+
+storage = (
+    MinIOStorage(
+        url=system_config.find_one({"name": "storage-minio-url"})["value"],
+        access_key=system_config.find_one({"name": "storage-minio-access-key"})[
+            "value"
+        ],
+        secret_key=system_config.find_one({"name": "storage-minio-secret-key"})[
+            "value"
+        ],
+        bucket=system_config.find_one({"name": "storage-minio-bucket"})["value"],
+    )
+    if system_config.find_one({"name": "storage-type"})["value"] == "minio"
+    else OSSStorage(
+        url=system_config.find_one({"name": "storage-oss-url"})["value"],
+        access_key=system_config.find_one({"name": "storage-oss-access-key"})["value"],
+        secret_key=system_config.find_one({"name": "storage-oss-secret-key"})["value"],
+    )
+)
+
+
+@router.post("/upload")
+async def upload_file(
+    file: bytes, filename: str, filetype: str, sid: str = Cookie(None)
+):
+    """
+    Upload a file
+    上传文件
+
+    Args 参数:
+    - file: File content 文件内容
+    - filename: File name 文件名
+    - filetype: File type (can be Normal or Data) 文件类型【可以是 Normal（普通文件） 或 Data（题目数据）】
+    """
+    if not db["sessions"].find_one({"sid": sid}):
+        raise HTTPException(status_code=401, detail="Unauthorized")
+    if not common.db.check_permission(db, sid=sid, permission="upload-file"):
+        raise HTTPException(status_code=403, detail="Permission denied, upload-file")
+
+    file_size = 0
+    tmp_filename = f"/tmp/{common.get_timestamp()}"
+    with open(tmp_filename, "wb") as f:
+        while chunk := await file.read(10 * 1024 * 1024):
+            file_size += len(chunk)
+            if file_size > int(
+                system_config.find_one({"name": "max-file-size"})["value"]
+            ):
+                raise HTTPException(status_code=413, detail="File too large")
+            f.write(chunk)
+
+    hash_filename = common.get_file_hash(tmp_filename)
+    if db["files"].find_one({"hash_filename": hash_filename}):
+        os.remove(tmp_filename)
+        db["files"].insert_one(
+            {
+                "filename": filename,
+                "hash_filename": hash_filename,
+                "size": file_size,
+                "filetype": filetype,
+                "timestamp": common.get_timestamp(),
+            }
+        )
+        return {"status": "success"}
+
+    db["files"].insert_one(
+        {
+            "filename": filename,
+            "hash_filename": hash_filename,
+            "size": file_size,
+            "filetype": filetype,
+            "timestamp": common.get_timestamp(),
+        }
+    )
+    with open(tmp_filename, "rb") as f:
+        storage.upload(f.read(), hash_filename)
+    os.remove(tmp_filename)
+
+    return {"status": "success"}
+
+
+@router.get("/{filename}")
+async def download_file(filename: str, sid: str = Cookie(None)):
+    """
+    Download a file
+    下载文件
+    """
+    file_record = db["files"].find_one({"filename": filename})
+    if file_record is None:
+        raise HTTPException(status_code=404, detail="File not found")
+    hash_filename = file_record["hash_filename"]
+
+    user = db["sessions"].find_one({"sid": sid})
+    if user is None and file_record["filetype"] == "Data":
+        raise HTTPException(status_code=401, detail="Unauthorized")
+
+    if (
+        not common.db.check_permission(db, user, permission="download-data-file")
+        and file_record["filetype"] == "Data"
+    ):
+        raise HTTPException(status_code=403, detail="Permission denied, download-data-file")
+    presigned_url = storage.generate_presigned_url(hash_filename)
+
+    response = RedirectResponse(url=presigned_url)
+    response.headers["Content-Disposition"] = f"attachment; filename={filename}"
+    return response
